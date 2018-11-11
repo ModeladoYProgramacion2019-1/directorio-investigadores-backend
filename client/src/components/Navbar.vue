7fe4f1922e72bc18ae9f5bff5770a7b136209c0a
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-11-11 16:48:35 -0600

    [C.0.1.0][Feature] Home page.

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light  sticky-top ">
+      <div class="container">
+        <a class="navbar-brand" href="/">Red Nacional de Investigación</a>
+        <form class="form-inline">
+          <router-link to="/search">
+            <a class="btn btn-secondary" href="/search">Búsqueda</a>
+          </router-link>
+          <router-link to="/login">
+            <a class="btn btn-primary btn-margin-left" href="#">Acceder</a>
+          </router-link>
+        </form>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style>
+    nav {
+        background-color: rgba(250, 250, 250, 0.88);
+    }
+</style>
---
+++ client/src/components/Start.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <Navbar/>
+
+    <header class ="landingHead text-white text-center">
+      <h1 class="mb-5">Bienvenido a la Red Nacional de Investigación</h1>
+      <h5> Haga click en una entidad en el mapa para realizar una búsqueda.</h5>
+    </header>
+
+    <Footer/>
+
+  </div>
+</template>
+
+<script>
+    import Navbar from '@/components/Navbar.vue'
+    import Footer from '@/components/Footer.vue'
+
+    export default {
+        name: 'Start',
+        components: {
+            Navbar,
+            Footer
+        }
+}
+</script>
+
+<style>
+    h5 {
+        font-family: 'Avenir', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-weight: 700;
+    }
+    header.landingHead {
+        position: relative;
+        background: url('../assets/images/background.png') no-repeat center center;
+        background-size: cover;
+        background-attachment : fixed;
+        padding-top: 8rem;
+        padding-bottom: 8rem;
+    }
+    div.mapDiv {
+        background-color: #342F2F;
+        width: 100%;
+    }
+    img{
+        margin-top: 10px;
+        object-fit: scale-down;
+        max-width: 100%;
+    }
+    .btn-margin-left {
+        margin-left: 15px;
+    }
+
+</style>
