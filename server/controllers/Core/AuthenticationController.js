a4d189d2ee98c64131e06ebcc87216605f12ddaf
Cesar Farrera <cfarrera@nerds.ai>
2018-11-20 13:22:39 -0600

    [1.9.0][Feature] Implemented AuthenticationController/acceptVerification

--- server/controllers/Core/AuthenticationController.js
+++ server/controllers/Core/AuthenticationController.js
@@ -7,6 +7,7 @@ var bcrypt = require('bcrypt');
 
 let Authentication = function(){
     this.signUp = signUp;
+    this.acceptVerification = acceptVerification;
 }
 
 let signUp = async function(req, res){
@@ -43,15 +44,70 @@ let signUp = async function(req, res){
             nombre: nueva.get("nombre")+" "+nueva.get("apellido"),
             correo: contactoNuevo.get("correo_personal"),
             contraseña: decoded.contraseña,
-            persona_id: nueva.get("id")
+            persona_id: nueva.get("persona_id")
         }
-        var signupToken = jwt.sign(tokenData, process.env.JWT_key, {expiresIn: "3m"});
+        var signupToken = jwt.sign(tokenData, process.env.JWT_key, {expiresIn: "90 days"});
         Email.sendVerifyAccount(nueva, contactoNuevo.get("correo_personal"), signupToken);
 
         return res.json({
             success:true,
             code: 200
         });
+    }catch(error){
+        console.log(error);
+        return res.json({
+            success: false,
+            code: 500,
+            error: error
+        });
+    }
+}
+
+let acceptVerification = async function(req, res){
+    try{
+        if(!req.query.token){
+            return res.json({
+                success: false,
+                code: 400,
+                error: "No se recibió una ficha"
+            });
+        }
+
+        var decoded = jwt.verify(req.query.token, process.env.JWT_key);
+        console.log(decoded);
+        if(!decoded.persona_id){
+            return res.json({
+                success: false,
+                code: 400,
+                error: "No se recibió un id de persona"
+            });
+        }
+
+        var toVerify = await Models.Persona.findOne({
+            where: {
+                persona_id: decoded.persona_id
+            }
+        });
+
+        if(!toVerify){
+            return res.json({
+                success: false,
+                code: 400,
+                error: "No se encontró la persona"
+            });
+        }
+
+        await toVerify.update({
+            is_verified: true
+        });
+
+        return res.json({
+            success: true,
+            code: 200,
+            resource: toVerify
+        })
+
+
     }catch(error){
         console.log(error);
         return res.json({
