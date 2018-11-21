066a573144dbcd812c3053012bd9c4c4ba27e9e4
lnataliamg <lnataliamg@ciencias.unam.mx>
2018-11-21 09:51:54 -0600

    [C.0.8.0][Feature] Created SignUp view

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import axios from 'axios'
 
 import BootstrapVue from 'bootstrap-vue'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
