a913f59a6ee379bf16825288205b7569efc01bf8
lautimartner <lautimartner@ciencias.unam.mx>
2018-12-10 12:00:36 -0600

    Simple Search Finished

--- server/controllers/Core/SearchController.js
+++ server/controllers/Core/SearchController.js
@@ -10,24 +10,68 @@ let Search = function(){
 }
 
 let simpleSearch = async function(req, res){
-    let searchedFor = req.body.data;
-
-    let dataFound = {
-        Persona: [],
-        Institucion: [],
-        Sede: [],
-        Articulo: []
-    }
-
-    var sedesFound = Models.Sede.findAlll({
-        where: {
-            name: {[Op.like]: searchedFor}
+    try{
+        let searchedFor = req.body.data;
+        let dataFound = {
+            Persona: [],
+            Grupo: [],
+            Sede: [],
+            Articulo: []
         }
-    });
 
+        var seatsFound = Models.Sede.findAll({
+            where: {
+                nombre: {[Op.like]: searchedFor}
+            }
+        });
+        
+        var articlesFound = Models.Articulo.findAll({
+            where: {
+                [Op.or]: 
+                    [{titulo: {[Op.like]: searchedFor}},
+                    {revista: {[Op.like]: searchedFor}}]                
+            }
+        });
+        
+        var personsFound = Models.Persona.findAll({
+            where: {
+                [Op.or]: 
+                    [{nombre: {[Op.like]: searchedFor}},
+                    {apellido: {[Op.like]: searchedFor}}]
+                // Buscar correo en la tabla direccion        
+            }
+        });
+        
+        var groupsFound = Models.Grupo.findAll({
+            where: {
+                nombre: {[Op.like]: searchedFor}
+            }
+        });
+        
+        dataFound.Persona.push(personsFound);
+        dataFound.Grupo.push(groupsFound);
+        dataFound.Sede.push(seatsFound);
+        dataFound.Articulo.push(articlesFound);
+        
+        return res.json({
+            success: true,
+            code: 200,
+            resource: dataFound
+            
+        })}catch(error){
+            console.log(error);
+            return res.json({
+                success: false,
+                code: 500,
+                error: error
+        });
+        }
+            
 }
 
 let advancedSearch = function(req, res){
+    let searchedFor = req.body.data;
+    let 
     return res.json({
         success: true,
         status: "In progress"
