2dfaaeaf2c4ccf6c47fe9981d97b0b0eafadf785
lnataliamg <lnataliamg@ciencias.unam.mx>
2018-12-12 22:41:58 -0600

    [C.1.1.0] Add validator to input

--- client/src/components/Editor.vue
+++ client/src/components/Editor.vue
@@ -11,39 +11,45 @@
           <b-tab title="Información personal" active>
 
             <div class="text-left mb-3">
-              <h3>Cuenta</h3>
+              <h3>Información personal</h3>
               <hr>
             </div>
 
 
             <div class = "text-left mb-2">
-              <label for="inputName" class="control-label">Nombre</label>
-              <input v-validate="'required'" name="name" type="text" class="form-control col-sm-5" value="NAME" ref="name">
+              <label class="control-label">Nombre</label>
+              <input v-validate="'required'" name="name" type="text" class="form-control col-sm-5" value="NOMBRE" ref="name">
               <span style="color:red">{{ errors.first('name') }}</span>
             </div>
 
             <div class = "text-left mb-2">
               <label class="control-label">Apellido</label>
               <input v-validate="'required'" name="lastName" type="text" class="form-control col-sm-5" value="APELLIDO" ref="name">
-              <span style="color:red">{{ errors.first('last_name') }}</span>
+              <span style="color:red">{{ errors.first('lastName') }}</span>
             </div>
-            
+
             <div class = "text-left mb-2">
-              <label for="inputEmail" class="control-label">Correo electrónico</label>
+              <label class="control-label">Correo electrónico</label>
               <input v-validate = "'required|email'"  name= "email" type="text" class="form-control col-sm-5"  value="EMAIL@EXAMPLE.COM">
               <span style="color:red">{{ errors.first('email') }}</span>
             </div>
 
             <div class = "text-left mb-2">
-              <label for="input_field" class="control-label">Campo</label>
+              <label class="control-label">Campo</label>
               <input v-validate = "'required'"  name= "field" type="text" class="form-control col-sm-5"  value="CAMPO">
               <span style="color:red">{{ errors.first('field') }}</span>
             </div>
 
             <div class = "text-left mb-2">
-              <label for="input_field" class="control-label">Campo</label>
-              <input v-validate = "'required'"  name= "field" type="text" class="form-control col-sm-5"  value="CAMPO">
-              <span style="color:red">{{ errors.first('field') }}</span>
+              <label class="control-label">Biografía</label>
+              <textarea v-validate = "'required'"  name= "biography" type="text" class="form-control col-sm-5" > BIOGRAFÍA </textarea>
+              <span style="color:red">{{ errors.first('biography') }}</span>
+            </div>
+
+            <div class = "text-left mb-2">
+              <label class="control-label">Currículum</label>
+              <textarea v-validate = "'required'"  name= "resume" type="text" class="form-control col-sm-5" > CURRICULUM </textarea>
+              <span style="color:red">{{ errors.first('resume') }}</span>
             </div>
 
 
@@ -53,106 +59,126 @@
 
           </b-tab>
 
-          <b-tab title="Información institucion">
 
+          <b-tab title="Información institución">
             <div class="text-left mb-3">
-              <h3>Información institucion </h3>
+              <h3>Información institución</h3>
               <hr>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputName" class="control-label">Nombre de la institucion</label>
-              <input v-validate="'required'" name="name_institution" type="text" class="form-control col-sm-5" value="NAMEINSTITUTION" ref="name_institution">
+              <label class="control-label">Nombre de la institución</label>
+              <input v-validate="'required'" name="name" type="text" class="form-control col-sm-5" value="NOMBRE" ref="name_institution">
               <span style="color:red">{{ errors.first('name') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="description" class="control-label">Descripcioón de la institucion</label>
-              <input v-validate="'required'" name="name_institution" type="text" class="form-control col-sm-5" value="DESCRIPCION" >
+              <label class="control-label">Descripción de la institución</label>
+              <input v-validate="'required'" name="description" type="text" class="form-control col-sm-5" value="DESCRIPCION" >
               <span style="color:red">{{ errors.first('description') }}</span>
             </div>
 
 
+            <div class = "text-left container mb-2">
+              <label class="control-label">Clave de la institución</label>
+              <input v-validate="'required|length:5'" name="key" type="text" class="form-control col-sm-5" value="CLAVE" >
+              <span style="color:red">{{ errors.first('key') }}</span>
+            </div>
+
+          </b-tab>
+
+          <b-tab title="Información sede">
+
+            <div class="text-left mb-3">
+              <h3>Información sede </h3>
+              <hr>
+            </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputName" class="control-label">Nombre</label>
-              <input v-validate="'required'" name="name" type="text" class="form-control col-sm-5" value="NAME" ref="name">
+              <label class="control-label">Nombre de la sede</label>
+              <input v-validate="'required'" name="name" type="text" class="form-control col-sm-5" value="NOMBRE" ref="name_institution">
               <span style="color:red">{{ errors.first('name') }}</span>
+            </div>
+
+            <div class = "text-left container mb-2">
+              <label class="control-label">Descripción de la sede</label>
+              <input v-validate="'required'" name="description" type="text" class="form-control col-sm-5" value="DESCRIPCION" >
+              <span style="color:red">{{ errors.first('description') }}</span>
             </div>
 
 
             <div class = "text-left container mb-2">
-              <label for="inputState" class="control-label">Estado</label>
+              <label class="control-label">Clave de sede</label>
+              <input v-validate="'required|length:5'" name="key" type="text" class="form-control col-sm-5" value="CLAVE" >
+              <span style="color:red">{{ errors.first('key') }}</span>
+            </div>
+
+
+            <div class = "text-left container mb-2">
+              <label class="control-label">Estado</label>
               <input v-validate = "'required|'" name= "state" type="text" value= "ESTADO "class="form-control col-sm-5" >
               <span style="color:red">{{ errors.first('state') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputCity" class="control-label">Ciudad</label>
-              <input v-validate = "'required|'" id= "city" class="form-control col-sm-5"  value="CIUDAD" >
+              <label class="control-label">Ciudad</label>
+              <input v-validate = "'required|'" name= "city" class="form-control col-sm-5"  value="CIUDAD" >
               <span style="color:red">{{ errors.first('city') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputTown" class="control-label">Municipio</label>
+              <label class="control-label">Municipio</label>
               <input v-validate = "'required|'"  name= "town" type="text" class="form-control col-sm-5"  value="MUNICIPIO"  required>
               <span style="color:red">{{ errors.first('town') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputSuburb" class="control-label">Colonia</label>
+              <label  class="control-label">Colonia</label>
               <input v-validate = "'required|'"  name= "suburb" type="text" class="form-control col-sm-5"  value="COLONIA">
               <span style="color:red">{{ errors.first('suburb') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="PC" class="control-label">Código Postal</label>
+              <label  class="control-label">Código Postal</label>
               <input v-validate = "'required|numeric'"  name= "PC" type="text" class="form-control col-sm-5"  value="1000"  required>
               <span style="color:red">{{ errors.first('PC') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputStreet" class="control-label">Calle</label>
+              <label class="control-label">Calle</label>
               <input v-validate = "'required|'"  name= "street" type="text" class="form-control"  value="CALLE"  required>
               <span style="color:red">{{ errors.first('street') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputNumE" class="control-label">Número Exterior</label>
-              <input v-validate = "'required|numeric'"  name= "num_ext" type="text" class="form-control col-sm-5"  value="1"  required>
-              <span style="color:red">{{ errors.first('num_ext') }}</span>
+              <label  class="control-label">Número Exterior</label>
+              <input v-validate = "'required|numeric'"  name= "numE" type="text" class="form-control col-sm-5"  value="1"  required>
+              <span style="color:red">{{ errors.first('numE') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputNumI" class="control-label">Número interior</label>
-              <input v-validate = "'numeric'"  name= "num_int" type="text" class="form-control col-sm-5"  value="1"  required>
-              <span style="color:red">{{ errors.first('num_int') }}</span>
+              <label class="control-label">Número interior</label>
+              <input name= "num_int" type="text" class="form-control col-sm-5"  value="1"  required>
+
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputBuild" class="control-label">Edificio</label>
+              <label  class="control-label">Edificio</label>
               <input v-validate = "'required'"  name= "building" type="text" class="form-control col-sm-5"  value="EDIFICIO"  required>
               <span style="color:red">{{ errors.first('building') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputFloor" class="control-label">PISO</label>
+              <label class="control-label">Piso</label>
               <input v-validate = "'required'"  name= "floor" type="text" class="form-control col-sm-5"  value="PISO"  required>
               <span style="color:red">{{ errors.first('floor') }}</span>
             </div>
 
             <div class = "text-left container mb-2">
-              <label for="inputCubicle" class="control-label">Cubículo</label>
+              <label class="control-label">Cubículo</label>
               <input v-validate = "'required'"  name= "cubicle" type="text" class="form-control col-sm-5"  value="CUBICULO"  required>
               <span style="color:red">{{ errors.first('cubicle') }}</span>
             </div>
-
-
-
-
-
-
-
 
             <div class = "text-left">
               <button type="button" class="btn btn-primary">Guardar cambios</button>
@@ -160,9 +186,7 @@
           </b-tab>
 
 
-          <b-tab title="Tab 4">
-            Tab Contents 3
-          </b-tab>
+
 
           <b-tab  href="#/password" title="Contraseña">
 
@@ -173,7 +197,7 @@
             </div>
 
             <div class="form-group container text-left">
-              <label for="inputNewPas" class="control-label">Contraseña actual</label>
+              <label class="control-label">Contraseña actual</label>
               <input v-validate="'required'" name="new_password" type="password" class="form-control col-sm-5"  ref="new_password">
               <span style="color:red">{{ errors.first('new_password') }}</span>
               <a href="#"  role="button" >¿Olvidaste tu contraseña?</a>
@@ -182,13 +206,13 @@
 
 
             <div class="form-group container text-left">
-              <label for="inputName" class="control-label">Nueva contraseña</label>
+              <label class="control-label">Nueva contraseña</label>
               <input v-model = "password"v-validate="'required'" name="password" type="password" class="form-control col-sm-5"  ref="password">
               <span style="color:red">{{ errors.first('password') }}</span>
             </div>
 
             <div class="form-group container text-left">
-              <label for="inputName" class="control-label">Confirmar contraseña</label>
+              <label class="control-label">Confirmar contraseña</label>
               <input  v-validate="'required|confirmed:password'" name="password_confirmation" type="password" class="form-control col-sm-5" data-vv-as="password">
               <span style="color:red">{{ errors.first('password_confirmation') }}</span>
             </div>
@@ -199,9 +223,7 @@
 
 
           </b-tab>
-          <b-tab title="Tab 6">
-            Tab Contents 3
-          </b-tab>
+
         </b-tabs>
       </b-card>
     </div>
--- client/src/validator.js
+++ client/src/validator.js
@@ -11,29 +11,86 @@ const dict = {
       required: () => 'El nombre no puede estar vacío.'
     },
     lastName: {
-      required: () => 'El apellido no puede estar vacío'
+      required: () => 'El apellido no puede estar vacío.'
     },
     email: {
       email: function () {
         return "Email inválido. Por favor inténtalo de nuevo."
       },
-      required: () => 'El email no puede estar vacío'
+      required: () => 'El email no puede estar vacío.'
 
     },
     password: {
-      required: () => 'La contraseña no puede estar vacía'
+      required: () => 'La contraseña no puede estar vacía.'
 
     },
     new_password: {
-      required: () => 'La contraseña no puede estar vacía'
+      required: () => 'La contraseña no puede estar vacía.'
 
     },
     password_confirmation: {
       confirmed: function () {
-        return "Las contraseñas no son iguales"
+        return "Las contraseñas no son iguales."
       },
-        required: () => 'La confirmación de la contraseña no puede estar vacía'
+        required: () => 'La confirmación de la contraseña no puede estar vacía.'
+    },
+
+    field: {
+      required: () => 'El campo no puede estar vacío.'
+    },
+    resume: {
+      required: () => 'El currículum no puede estar vacío.'
+    },
+    biography: {
+      required: () => 'La biografía no puede estar vacía.'
+    },
+    description: {
+      required: () => 'La descripción no puede estar vacía.'
+    },
+    key: {
+      length: function(){
+        return "Se requiere un máximo de 5 caracteres."
+      },
+      required: () => 'La clave no puede estar vacía.'
+    },
+    state: {
+      required: () => 'Se requiere elegir un estado.'
+    },
+    city: {
+      required: () => 'Se requiere escribir una ciudad.'
+    },
+    town: {
+      required: () => 'Se requiere escribir un municipio.'
+    },
+    suburb: {
+      required: () => 'Se requiere escribir una colonia.'
+    },
+    PC: {
+      numeric: function(){
+        return "Formato inválido, intenta escribiendo números."
+      },
+      required: () => 'Se requiere escribir un código postal.'
+    },
+    street: {
+      required: () => 'Se requiere escribir una calle.'
+    },
+    numE: {
+      numeric: function(){
+        return "Formato inválido, intenta escribiendo números."
+      },
+      required: () => 'Se requiere escribir una calle.'
+    },
+    building: {
+      required: () => 'Se requiere escribir el nombre o número de edificio.'
+    },
+    floor: {
+      required: () => 'Se requiere escribir el número de piso'
+    },
+    cubicle: {
+      required: () => 'Se requiere escribir el número o nombre del cubículo'
     }
+
+
 
   }
 };
